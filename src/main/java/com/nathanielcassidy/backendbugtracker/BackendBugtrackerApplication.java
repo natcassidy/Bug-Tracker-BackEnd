a965a2c638e6d43c9ae2ff8cb8c08c43a35c6e5a
natcassidy <nat.cassidy865@gmail.com>
2021-05-29 17:03:22 -0300

    Backend commit

---
+++ src/main/java/com/nathanielcassidy/backendbugtracker/BackendBugtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nathanielcassidy.backendbugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBugtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathanielcassidy/backendbugtracker/BackendBugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanielcassidy.backendbugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBugtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
